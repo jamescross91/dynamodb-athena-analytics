d16e2edebb67d1f96f1c0c3737e01e0081f99afd
jamescross91 <james_cross91@hotmail.com>
2017-09-23 00:55:50 +0100

    Adding firehose to s3 delivery stream, firehose producer

--- dynamo-stream-handler.js
+++ dynamo-stream-handler.js
@@ -1,10 +1,13 @@
 'use strict';
 const aws = require('aws-sdk');
+var firehoser = require('firehoser')
 
 module.exports.write_to_kinesis = (event, context, callback) => {
+  let firehose = new firehoser.DeliveryStream('dynamo-s3-delivery-stream');
+
   console.log(JSON.stringify(event, null, 2));
     event.Records.forEach(function(record) {
-        console.log('DynamoDB Record: %j', record.dynamodb.NewImage);
+        firehose.putRecord(JSON.stringify(record.dynamodb.NewImage, null, 2));
     });
 };
 

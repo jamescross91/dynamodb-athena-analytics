e64226a9ecc1108a023e1f6fd4517a9b28297ea6
jamescross91 <james_cross91@hotmail.com>
2017-09-23 14:35:40 +0100

    Ahmed

--- dynamo-stream-handler.js
+++ dynamo-stream-handler.js
@@ -1,13 +1,18 @@
 'use strict';
 const aws = require('aws-sdk');
 var firehoser = require('firehoser')
+var attr = require('dynamodb-data-types').AttributeValue;
+var stringify = require('csv-stringify');
 
 module.exports.write_to_kinesis = (event, context, callback) => {
   let firehose = new firehoser.DeliveryStream('dynamo-s3-delivery-stream');
 
-  console.log(JSON.stringify(event, null, 2));
-    event.Records.forEach(function(record) {
-        firehose.putRecord(JSON.stringify(record.dynamodb.NewImage, null, 2));
-    });
+  console.log(event.Records.map(attr.unwrap).map(stringify))
+
+    // event.Records.forEach(function(record) {
+    //   var unwrapped = attr.unwrap(record.dynamodb.NewImage);
+    //   console.log('Writing ' + csv)
+    //   firehose.putRecord(csv);
+    // });
 };
 

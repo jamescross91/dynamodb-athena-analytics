b94febee1a13ba7ae1599b7adfbee763693574a0
jamescross91 <james_cross91@hotmail.com>
2017-09-23 15:33:55 +0100

    Write JSONL

--- dynamo-stream-handler.js
+++ dynamo-stream-handler.js
@@ -2,17 +2,13 @@
 const aws = require('aws-sdk');
 var firehoser = require('firehoser')
 var attr = require('dynamodb-data-types').AttributeValue;
-var stringify = require('csv-stringify');
 
 module.exports.write_to_kinesis = (event, context, callback) => {
   let firehose = new firehoser.DeliveryStream('dynamo-s3-delivery-stream');
-
-  console.log(event.Records.map(attr.unwrap).map(stringify))
-
-    // event.Records.forEach(function(record) {
-    //   var unwrapped = attr.unwrap(record.dynamodb.NewImage);
-    //   console.log('Writing ' + csv)
-    //   firehose.putRecord(csv);
-    // });
+    event.Records.forEach(function(record) {
+      var unwrapped = attr.unwrap(record.dynamodb.NewImage);
+      console.log('Writing ' + JSON.stringify(unwrapped))
+      firehose.putRecord(JSON.stringify(unwrapped));
+    });
 };
 
